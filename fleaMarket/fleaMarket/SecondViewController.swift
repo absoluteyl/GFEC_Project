2f249a5b5d52a3e5c67176185ff550cad4f26caa
ElsaLau <chinbo@gmail.com>
2016-05-11 09:20:24 +0800

    Create Xcode project for iOS app

---
+++ fleaMarket/fleaMarket/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  fleaMarket
+//
+//  Created by Yuning Lau on 2016/5/11.
+//  Copyright © 2016年 absoluteyl. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
