91e085ddaa019a9d47e3d08867a87627bcf282a9
kyujyokei <kyujyokei@gmail.com>
2016-06-03 18:18:26 +0800

    Add camera & photo library on Tab 3 main / Map on Tab 5 But the caera need a device to be test on.

---
+++ iOS/fleaMarket/TabStoryReferenceSample/FifthTabViewController.swift
@@ -0,0 +1,67 @@
+//
+//  FifthTabViewController.swift
+//  fleaMarket
+//
+//  Created by Kero on 2016/6/3.
+//  Copyright © 2016年 ColorKit. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class FifthTabViewController: UIViewController , MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    
+    @IBOutlet weak var map: MKMapView!
+    
+    let locationManager = CLLocationManager() // get user's location
+    
+    var location: CLLocation!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.locationManager.delegate = self
+        
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest // get locations as accurate as possible
+        
+        self.locationManager.requestWhenInUseAuthorization() // get locations only when you are using the app
+        
+        self.locationManager.startUpdatingLocation() // turn on location manager to go look for location
+        
+        self.map.showsUserLocation = true
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.last // get the last location
+        let center = CLLocationCoordinate2D(latitude: (location?.coordinate.latitude)! , longitude: (location?.coordinate.longitude)!) // center to location
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta:  0.1, longitudeDelta: 0.1)) // zoom the map
+        
+        self.map.setRegion(region, animated: true)
+        
+        self.locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("Errors: " + error.localizedDescription) // should it trigger errors, this will put error messages to the debugger
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
