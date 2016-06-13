93ebbb161c7af7297d9f44f06248234b4b6f303d
ElsaLau@c9 <chinbo@gmail.com>
2016-06-13 06:14:45 +0000

    Extend acceptable length of category name

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
     has_many :subcategories, :class_name => "Category", :foreign_key => "parent_id", :dependent => :destroy
     belongs_to :parent_category, :class_name => "Category"
-    validates :name, presence: true, length: {minimum: 3, maximum: 25}
+    validates :name, presence: true, length: {minimum: 3, maximum: 30}
 end
