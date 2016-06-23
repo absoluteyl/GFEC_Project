8f07279e50bb76f3e9bfa90dec79bb01da0e8c76
ElsaLau@c9 <chinbo@gmail.com>
2016-06-23 08:27:20 +0000

    Change lat&long type in migration file

--- db/migrate/20160623080418_add_coordinate_to_location.rb
+++ db/migrate/20160623080418_add_coordinate_to_location.rb
@@ -1,6 +1,6 @@
 class AddCoordinateToLocation < ActiveRecord::Migration
   def change
-    add_column :locations, :lat, :double
-    add_column :locations, :long, :double
+    add_column :locations, :lat, :float
+    add_column :locations, :long, :float
   end
 end
