1ce4951c65762504860310ec0eedcc8d31f47dba
ElsaLau@c9 <chinbo@gmail.com>
2016-06-29 07:48:55 +0000

    Remove user routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'about' => 'welcome#about'
   get 'contact' => 'welcome#contact'
+  
   #Merchandises Routes
   resources :merchandises
   
@@ -10,23 +11,23 @@ Rails.application.routes.draw do
   resources :categories, except: [:destroy]
   
   #Users Routes
-  resources :users, except: [:new] do
-    member do
-      resources :locations, except: [:show]
-    end
-  end
-  #new user route
-  get 'signup', to: 'users#new'
+  # resources :users, except: [:new] do
+  #   member do
+  #     resources :locations, except: [:show]
+  #   end
+  # end
+  # #new user route
+  # get 'signup', to: 'users#new'
   
   #Sessions Routes
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  # get 'login', to: 'sessions#new'
+  # post 'login', to: 'sessions#create'
+  # delete 'logout', to: 'sessions#destroy'
   
   #API Routes
   namespace :api do
     resources :merchandises
-    resources :users
+    # resources :users
     resources :locations, except: [:show]
     resources :categories, except: [:destroy]
   end
