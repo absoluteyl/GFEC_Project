aad3dc4b66408e36f0584bd2098fec2835bf5042
ElsaLau@c9 <chinbo@gmail.com>
2016-06-29 08:03:00 +0000

    drop users and move all related files to bak

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160628015357) do
+ActiveRecord::Schema.define(version: 20160629075501) do
 
   create_table "api_keys", force: :cascade do |t|
     t.string   "api_key"
@@ -69,20 +69,6 @@ ActiveRecord::Schema.define(version: 20160628015357) do
   create_table "subcategories", force: :cascade do |t|
     t.string  "name"
     t.integer "category_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "email"
-    t.string   "mobile"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "password_digest"
-    t.boolean  "admin",               default: false
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
   end
 
 end
