4c2e4714397dd86ef8900cfca8f7e5abc1370022
ElsaLau@c9 <chinbo@gmail.com>
2016-06-30 08:52:01 +0000

    Update category api

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -1,18 +1,16 @@
-module Api
-   class CategoriesController < ApplicationController
-      def index
-        @categories = Category.all
-        if name = params[:name]
-            @categories = @categories.where(name: name)
-        end
-        if id = params[:id]
-          @categories = @categories.where(id: id)
-        end
-        
-        render status: 200, json: {
-        status: "OK", 
-        categories: @categories
+class Api::CategoriesController < Api::ApiController
+  def index
+    @categories = Category.all
+    if name = params[:name]
+        @categories = @categories.where(name: name)
+    end
+    if id = params[:id]
+      @categories = @categories.where(id: id)
+    end
+    
+    render status: 200, json: {
+    status: "OK", 
+    categories: @categories
     }.to_json
-      end
-   end
+  end
 end
