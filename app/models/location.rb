6114973c9bd7dfebb708ae76cdf10c16650b1544
ElsaLau@c9 <chinbo@gmail.com>
2016-07-05 07:53:46 +0000

    Fix syntax error in location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,8 @@
 class Location < ActiveRecord::Base
-    belongs_to :user
-    belongs_to :city
-    has_many :merchandises, through: :users
-    validates :user_id, presence: true
-    validates :lat, presence: true
-    validates :long, presence: true
+  belongs_to :user
+  belongs_to :city
+  has_many :merchandises, through: :users
+  validates :user_id, presence: true
+  validates :lat, presence: true
+  validates :long, presence: true
+end
