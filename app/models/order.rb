2f7795a012a3508fda62d586554b913cf6d0668f
ElsaLau@c9 <chinbo@gmail.com>
2016-07-05 09:37:26 +0000

    Add new order views

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,5 @@ class Order < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
   has_many :merchandises, through: :line_items
   
-  #PAYMENT_METHODS = [ "面交", "宅配（使用歐付寶付款）" ]
+  PAYMENT_METHODS = [ "面交", "宅配（使用歐付寶付款）" ]
 end
