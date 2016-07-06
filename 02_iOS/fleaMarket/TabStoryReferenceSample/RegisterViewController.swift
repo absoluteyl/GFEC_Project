8789573ec27511c187635fe9ab9316496d73aeee
kyujyokei <kyujyokei@gmail.com>
2016-07-06 21:29:58 +0800

    Roughly completed create account feature.

--- 02_iOS/fleaMarket/TabStoryReferenceSample/RegisterViewController.swift
+++ 02_iOS/fleaMarket/TabStoryReferenceSample/RegisterViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class RegisterViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     var uploadimage:UIImage?
+    
+    var statusReply:String?
 
     @IBOutlet weak var addPhotoButton: UIButton!
     
@@ -26,6 +28,7 @@ class RegisterViewController: UIViewController,UIImagePickerControllerDelegate, 
     
     @IBOutlet weak var CreateButton: UIButton!
     
+    @IBOutlet weak var pwCheckLabel: UILabel!
     @IBAction func addPhotoButtonAction(sender: UIButton) {
         
         let picker = UIImagePickerController()
@@ -37,6 +40,9 @@ class RegisterViewController: UIViewController,UIImagePickerControllerDelegate, 
     }
     
     @IBAction func CreateButtonAction(sender: UIButton) {
+        if userNameTextField.text != "" && emailTextField.text != "" && mobileTextField.text != "" && passwordTextField.text != "" && passwordTextField.text == passwordConfirmTextField.text {
+            self.performSegueWithIdentifier("goNext", sender: CreateButton)
+        }
     }
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
@@ -47,10 +53,31 @@ class RegisterViewController: UIViewController,UIImagePickerControllerDelegate, 
         }
     }
     
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+
+            let Destination : RegisteringViewController = segue.destinationViewController as! RegisteringViewController
+            Destination.userName = userNameTextField.text
+            Destination.email = emailTextField.text
+            Destination.mobile = mobileTextField.text
+            Destination.password = passwordTextField.text
+            Destination.uploadimage = uploadimage
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //CreateButton.enabled = false
+//        if userNameTextField.text != "" && emailTextField.text != "" && mobileTextField.text != "" && passwordTextField.text != "" && passwordTextField == passwordConfirmTextField {
+//            CreateButton.enabled = true
+//        }
         // Do any additional setup after loading the view.
+        
+        //for testing
+        userNameTextField.text = "Bubui"
+        passwordTextField.text = "buibui"
+        passwordConfirmTextField.text = "buibui"
+        emailTextField.text = "syugogetten0916@hotmail.com"
+        mobileTextField.text = "0909126126"
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -58,5 +85,5 @@ class RegisterViewController: UIViewController,UIImagePickerControllerDelegate, 
         // Dispose of any resources that can be recreated.
     }
     
-
+    
 }
