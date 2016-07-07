1a39e568c0ab0a07ecc4f75cbaff599ccc24cd66
ElsaLau@c9 <chinbo@gmail.com>
2016-07-07 08:00:56 +0000

    modify migration file for production

--- db/migrate/20160705125219_change_column_types_in_orders.rb
+++ db/migrate/20160705125219_change_column_types_in_orders.rb
@@ -1,7 +1,9 @@
 class ChangeColumnTypesInOrders < ActiveRecord::Migration
   def change
-    change_column :orders, :buyer, :integer
-    change_column :orders, :seller, :integer
+    execute 'ALTER TABLE listings ALTER COLUMN buyer TYPE integer USING (buyer::integer)'
+    execute 'ALTER TABLE listings ALTER COLUMN seller TYPE integer USING (seller::integer)'
+    #change_column :orders, :buyer, :integer
+    #change_column :orders, :seller, :integer
     rename_column :orders, :buyer, :buyer_id
     rename_column :orders, :seller, :seller_id
     add_column :orders, :location_id, :integer
